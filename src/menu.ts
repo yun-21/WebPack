4c7003030f47c5ec9384c8497e60b0372ef47b0e
shinjiyun <ranji12@naver.com>
2024-07-04 12:51:37 +0900

    typescript로 script작성하기

--- src/menu.ts
+++ src/menu.ts
@@ -1,90 +1,78 @@
-const root = document.getElementById('root') as HTMLDivElement;
+const root = document.getElementById('root')!;
 
-
-const anchorTags = (href:string, textNode:string):string => {
-  let result = `<a href="#${href}">${textNode}</a>`;
+const anchorTags = (href:string, text:string):string => {
+  let result = `<a href="#${href}">${text}</a>`;
   return result;
 }
 
 const liTags = (children:string):string => {
-  let result = `<li>${children}</li>`;
-  return result;
+  let result =`<li>${children}</li>`;
+  return result
 }
 
 
 interface BasicData {
-  jeamin: {
-    name: '재민',
-    backgroundColor: 'red',
+  jaemin: {
+    name:"재민",
+    backgroundColor:"red"
   },
-  hoyoung: {
-    name: "호영",
-    backgroundColor: "green",
+  ujin : {
+    name : "유진",
+    backgroundColor:"yellow"
   },
-  ujin: {
-    name: "유진",
-    backgroundColor: "yellow",
-  
+  jiyun: {
+    name : "지윤",
+    backgroundColor:"pink"
   },
-  jiyoon: {
-    name: "지윤",
-    backgroundColor: "purple",
-  },
-  ukjae: {
-    name: "욱재",
-    backgroundColor: "blue",
+  ukjae : {
+    name : "욱재",
+    backgroundColor:"blue"
   }
 }
 
-const basicData:BasicData = {
-  jeamin: {
-    name: '재민',
-    backgroundColor: 'red',
+const basicData:BasicData ={
+  jaemin: {
+    name:"재민",
+    backgroundColor:"red"
   },
-  hoyoung: {
-    name: "호영",
-    backgroundColor: "green",
+  ujin : {
+    name : "유진",
+    backgroundColor:"yellow"
   },
-  ujin: {
-    name: "유진",
-    backgroundColor: "yellow",
-  
+  jiyun: {
+    name : "지윤",
+    backgroundColor:"pink"
   },
-  jiyoon: {
-    name: "지윤",
-    backgroundColor: "purple",
-  },
-  ukjae: {
-    name: "욱재",
-    backgroundColor: "blue",
+  ukjae : {
+    name : "욱재",
+    backgroundColor:"blue"
   }
 }
 
-const totalElement = (object:BasicData):string => {
+const totalElement= (object:BasicData):string =>{
   let result = '';
-
   for (let key in object) {
+    // TypeScript가 key가 BasicData의 키인 것을 알게 함
     const value = object[key as keyof BasicData];
     result += liTags(anchorTags(key, value.name));
   }
   return result;
 }
 
-root.innerHTML = `
+root.innerHTML=`
 <ul id="menu">
   ${totalElement(basicData)}
 </ul>
 <main id="main-target"></main>
-`;
+`
 
-// ------------------------------------------------------------------
-// * HTML을 대신해서 만든 HTML 작성 코드
-// * React, CSR
+// -----------------------------------------------------------------------------//
+// 조립 컴포넌트 //
 
-const mainTarget = document.getElementById('main-target')!;
+const mainTarget = document.getElementById("main-target")!;
 console.log(window.location.hash);
 
-window.addEventListener('hashchange', () => {
+window.addEventListener('hashchange',()=>{
   const hash = window.location.hash;
   console.log(hash.slice(1));
   let test = basicData[hash.slice(1) as keyof BasicData];
@@ -96,4 +84,4 @@ window.addEventListener('hashchange', () => {
   }
   mainTarget.appendChild(div);
 
-});
+})
